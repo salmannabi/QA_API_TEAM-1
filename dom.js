e14a69d469b895b8cd5c9ab73c09a262a502436d
Jonah Atkinson <theJAtkinson@gmail.com>
2022-04-08 09:54:48 +0100

    Framework

---
+++ dom.js
@@ -0,0 +1,6 @@
+export const buttonRandom = document.getElementById(`buttonRandom`);
+export const buttonType = document.getElementById(`buttonType`);
+export const buttonParticipants = document.getElementById(`buttonParticipants`);
+export const buttonPrice = document.getElementById(`buttonPrice`);
+export const output = document.getElementById(`output`);
+export const input = document.getElementById(`input`);
---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict'
+// import the dom
+import * as DOM from './dom.js';
+
+//read random
+//read by type (drop down menu)
+//read by participants
+//read by max price
